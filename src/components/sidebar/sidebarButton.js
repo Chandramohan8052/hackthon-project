2e76db7e98c9df8110d0bfde706ee3cc1af1ade0
thechandramohan <71341790+Chandramohan8052@users.noreply.github.com>
2023-03-22 15:14:44 +0530

    change sidebar

--- src/components/sidebar/sidebarButton.js
+++ src/components/sidebar/sidebarButton.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import "./sidebarButton.css";
-export default function SidebarButton() {
+export default function SidebarButton(props) {
   return (
-    <div>
-      S
-    </div>
+   <Link typeof={props.to}>
+   <div>
+    {props.icon}
+    <p>{props.title}</p>
+   </div>
+   </Link>
   )
 }
