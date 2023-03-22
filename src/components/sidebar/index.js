70c3e6b697d32bb4ed48dc8b812d53fded83f20c
thechandramohan <71341790+Chandramohan8052@users.noreply.github.com>
2023-03-22 14:39:17 +0530

    changes sidebarbutton

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './sidebar.css';
+import SidebarButton from './sidebarButton';
+import { MdFavorite } from "react-icons/md";
+import { FaGripfire, FaPlay } from "react-icons/fa";
+import { FaSignOutAlt } from "react-icons/fa";
+import { IoLibrary } from "react-icons/io5";
+import { MdSpaceDashboard } from "react-icons/md";
 export default function Sidebar() {
   return (
     <div className='sidebar-container'>
-      Sidebar
+      <img src='https://images.unsplash.com/photo-1606663889134-b1dedb5ed8b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' className="profile-img" alt="profile" />
+      <div>
+        <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />}/>
+        <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
+        <SidebarButton title="Player" to="/player" icon={<FaPlay />}/>
+        <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite/>}/>
+        <SidebarButton title="Library" to="/" icon={<IoLibrary />}/>
+          
+      </div>
+      <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />}/>
     </div>
   )
 }
---
+++ src/components/sidebar/sidebarButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./sidebarButton.css";
+export default function SidebarButton() {
+  return (
+    <div>
+      S
+    </div>
+  )
+}
