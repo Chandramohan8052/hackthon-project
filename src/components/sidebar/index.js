fc88cbaa4e183174f55ab2944d399886a940acbd
thechandramohan <71341790+Chandramohan8052@users.noreply.github.com>
2023-03-23 15:30:34 +0530

    change all file

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import './sidebar.css';
-import SidebarButton from './sidebarButton';
+import React, { useState, useEffect } from "react";
+import "./sidebar.css";
+import SidebarButton from "./sidebarButton";
 import { MdFavorite } from "react-icons/md";
 import { FaGripfire, FaPlay } from "react-icons/fa";
 import { FaSignOutAlt } from "react-icons/fa";
 import { IoLibrary } from "react-icons/io5";
 import { MdSpaceDashboard } from "react-icons/md";
+import apiClient from "../../spotify";
+
 export default function Sidebar() {
+  const [image, setImage] = useState(
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdLAY3C19kL0nV2bI_plU3_YFCtra0dpsYkg&usqp=CAU"
+  );
+  useEffect(() => {
+    apiClient.get("me").then((response) => {
+      setImage(response.data.images[0].url);
+    });
+  }, []);
   return (
-    <div className='sidebar-container'>
-      <img src='https://images.unsplash.com/photo-1606663889134-b1dedb5ed8b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' className="profile-img" alt="profile" />
-      <div className='button'>
-        <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />}/>
+    <div className="sidebar-container">
+      <img src={image} className="profile-img" alt="profile" />
+      <div>
+        <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
         <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
-        <SidebarButton title="Player" to="/player" icon={<FaPlay />}/>
-        <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite/>}/>
-        <SidebarButton title="Library" to="/" icon={<IoLibrary />}/>
-          
+        <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
+        <SidebarButton
+          title="Favorites"
+          to="/favorites"
+          icon={<MdFavorite />}
+        />
+        <SidebarButton title="Library" to="/" icon={<IoLibrary />} />
       </div>
-      <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />}/>
+      <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />} />
     </div>
-  )
+  );
 }
--- src/screens/feed/feed.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function feed() {
-  return (
-    <div>
-      feed
-    </div>
-  )
-}
